89ae516a94e5c8362a75b78491a507d5296761db
גאון גדול <gwngdwl@gmail.com>
2026-01-25 20:52:51 +0200

    Update RSS feed URL in downloader script and configuration

--- rss.py
+++ rss.py
@@ -7,7 +7,7 @@ import os
 import time
 from pathlib import Path
 
-RSS_URL = "https://www.osimhistoria.com/osim-psychology/feed/podcast"
+RSS_URL = "https://www.spreaker.com/show/4227531/episodes/feed"
 
 DOWNLOAD_LAST_N = 3   # 👈 שנה ל־1 / 5 / 10 לפי הצורך
 DOWNLOAD_DIR = Path("downloads")
